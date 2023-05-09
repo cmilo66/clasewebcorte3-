15dbf08970e202568f47ff7d4fece82321be5665
winsignares <ingenieroinsignares@hotmail.com>
2023-05-08 20:44:05 -0500

    models

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask,  redirect, request, jsonify, json, session, render_template
 from config.bd import db, app, ma
 
+@app.route('/', methods=['GET'])
+def index():
+    return "Prueba"
 
 if __name__ == '__main__':
    # load_dotenv()
