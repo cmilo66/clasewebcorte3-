8de33af7371636a408bd2befeaec5eaa63327946
winsignares <ingenieroinsignares@hotmail.com>
2023-05-08 21:25:09 -0500

    bluprnt con pedidos

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from config.bd import db, app, ma
 
 from api.clientes import route_clientes
 from api.tiendas import route_tiendas
-
+from api.pedidos import route_pedidos
 app.register_blueprint(route_clientes, url_prefix="/api")
 
-
+app.register_blueprint(route_pedidos, url_prefix="/api")
 @app.route('/', methods=['GET'])
 def index():
     return "Prueba"
